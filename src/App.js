6927e9673a2c6c83badb552ee86760c25b27c288
sahil kumar <sahil511kumar@gmail.com>
2020-10-14 23:17:58 +0530

    shoppe view added bhofdike

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ import {Route,Switch,withRouter} from 'react-router-dom';
 import Shope from './components/shop/shop';
 import Contact from './components/contact';
 import About from './components/about';
+import {dishes} from './store'
 
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/shop" component={Shope} />
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact} />
-      </Switch>
-      <Footer />
-    </div>
-  );
-}
-
-export default withRouter(App);
+ export default withRouter( class App extends React.Component {
+  state = { 
+    dishes:dishes
+   }
+  render() { 
+    return ( 
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/shop" component={()=><Shope items={dishes}/>} />
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+     );
+  }
+})
+ 
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Grid, Paper } from '@material-ui/core';
 
-const Shope = () => {
+
+const style={
+    paper:{
+        height:'100vh',
+        color:'red',
+        background:'black',
+        position:'block'
+    }
+}
+
+const Item = ({item}) => {
     return ( 
-        <div>
-            <h1>items</h1>
-        </div>
+
+        <Card key={item.id} style={{maxWidth:345,height:350}} raised>
+            <CardActionArea>
+            <CardMedia
+                style={{height:140}}
+                image={item.image}
+                title={item.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                {item.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                {item.description}
+                </Typography>
+            </CardContent>
+            </CardActionArea>
+      </Card>
+     );
+}
+ 
+
+
+
+const Shope = ({items}) => {
+
+    const products=items.map((item)=>{
+        return(
+            <Grid item style={{margin:15}}>
+                <Item item={item}/>
+            </Grid>
+        )
+    })
+
+    return ( 
+        <Grid container  spacing={2}>
+            <Grid item xs={3} >
+                <Paper style={style.paper}>
+                    <Typography>
+                        yaha ayenge filters
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid container xs={9} >
+                {products}
+            </Grid>
+        </Grid>
      );
 }
  
