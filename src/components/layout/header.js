b5ffe96d2644300e760b670a34cf5e4e8ce7493f
spyder204 <vibhortiwari11@gmail.com>
2020-10-14 23:17:34 +0530

    Changed header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -3,35 +3,41 @@ import { AppBar,Button,Toolbar,Typography} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 import SignIn from './signinModal';
 
+const headerDiv = {
+  marginLeft:"68%"
+}
+
 const style={
-    padding: "20px 30px",
+    backgroundColor:"#eceff1",
+    borderRadius:"30px",
+    marginRight:"5px",
+    padding: "10px 15px",
     textAlign: "center",
     textDecoration: "none",
     display:" inline-block",
     cursor:'pointer',
-    color:'black'
+    color:'black',
+    fontSize:'12px',
+    fontWeight:"600"
+
 }
 
 const Header = () => {
 
     return ( 
       <>
-        <AppBar color="transparent" position="static">
-          <Toolbar style={{height:'100%'}} >
-            <Typography variant="h6" style={{ flex:1}}>
-              social media icons
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        
         <AppBar color="transparent" position="sticky">
         <Toolbar >
-          <Typography variant="h6" style={{ flex:1}}>
-            Our Luvly Organ!Kraft 
+          <Typography variant="h6" style={{ flex:0}}>
+            Organikart 
           </Typography>
+            <div style={headerDiv}>
             <Link to="/"><div style={style}>HOME</div></Link>
             <Link to="/about"><div style={style}>ABOUT</div></Link>
             <Link to="/shop"><div style={style}>SHOP</div></Link>
             <Link to="/contact"><div style={style}>CONTACT</div></Link>
+            </div>
             <SignIn style={style}/>
           </Toolbar>
         </AppBar>
