4ad62404145bb8efa24f6a2bbb42f93f015fab05
spyder204 <vibhortiwari11@gmail.com>
2021-09-27 14:23:28 +0530

    removed redundant code from userProfile

--- src/components/shop/all-order/userProfile/userAddress/userAddress.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState} from 'react';
-const UserAddress = () =>{
-    return(
-        <div>
-            User Address 
-        </div>
-    )
-}
-export default UserAddress;
--- src/components/shop/all-order/userProfile/userProfile.jsx
+++ src/components/shop/all-order/userProfile/userProfile.jsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faShoppingBag ,faUser, faShoppingCart, faAddressCard } from '@fortawesome/free-solid-svg-icons'
 import PersonalDetails from './personalDetails/personalDetails';
 import UserOrders from './userOrders/userOrders';
-import UserAddress from './userAddress/userAddress';
 
 
 const UserProfile = () =>{
@@ -13,7 +12,6 @@ const UserProfile = () =>{
     const [viewItem, setViewItem] = useState({
         1:true,
         2:false,
-        3:false
     });
 
     return(
@@ -24,7 +22,6 @@ const UserProfile = () =>{
                     setViewItem({
                         1:true,
                         2:false,
-                        3:false
                     })
                 }}
             >
@@ -36,31 +33,17 @@ const UserProfile = () =>{
                     setViewItem({
                         1:false,
                         2:true,
-                        3:false
                     })
                 }}
             >
                 <FontAwesomeIcon  style = {{marginRight:'10px'}}icon = {faShoppingBag}/>
                 Orders
             </div>
-           <div className = {`userLeftPane-item activeItem-${viewItem[3]}`}
-                onClick = {() =>{
-                    setViewItem({
-                        1:false,
-                        2:false,
-                        3:true
-                    })
-                }}
-            >
-                <FontAwesomeIcon style = {{marginRight:'10px'}} icon = {faAddressCard}/>
-                Address
-            </div>
            </Grid>
 
            <Grid className = 'rightPane-item' item xs = {8}>
             {viewItem[1] && <PersonalDetails />}
             {viewItem[2] && <UserOrders/>}
-            {viewItem[3] && <UserAddress />}
            </Grid>
 
 
