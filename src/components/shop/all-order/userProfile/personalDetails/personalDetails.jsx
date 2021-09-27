2411f0e04dd6e89d71260a62d7ec8588dc33541f
spyder204 <vibhortiwari11@gmail.com>
2021-09-27 14:21:27 +0530

    user personal details ui

--- src/components/shop/all-order/userProfile/personalDetails/personalDetails.jsx
+++ src/components/shop/all-order/userProfile/personalDetails/personalDetails.jsx
@@ -3,29 +3,98 @@ import { Button, Grid, Paper, Typography, TextField } from "@material-ui/core";
 import "./personalDetailsStyles.css";
 
 const PersonalDetails = () => {
- 
-  const inputs = ['First Name', 'Last Name', 'Phone', 'Street Address', 'City', 'Zip'];
+  const [userDetails, setUserDetails] = useState({
+    "First Name": "",
+    "Last Name": "",
+    Phone: "",
+    "Street Address": "",
+    City: "",
+    Zip: "",
+  });
+  const handleSubmit = () => {
+    console.log(userDetails);
+  };
+
   return (
     <Grid container className="personalDetailsContainer" spacing={0}>
-       {
-           inputs.map((field, index) => (
-            <Grid item xs={5}>
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label = {field}
-                    maxRows={4}
-                    variant = 'outlined'
-                    className = 'userInput'
-                />
-            </Grid>
-        ))
-       }
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="First Name"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, "First Name": e.target.value })
+          }
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Last Name"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, "Last Name": e.target.value })
+          }
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Phone"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, "First Name": e.target.value })
+          }
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Street Address"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, "Street Address": e.target.value })
+          }
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="City"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, City: e.target.value })
+          }
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Zip Code"
+          maxRows={4}
+          variant="outlined"
+          className="userInput"
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, "Zip Code": e.target.value })
+          }
+        />
+      </Grid>
 
-       <Grid item xs = {10}>
-           <Button variant="contained" >
-               Save
-           </Button>
-       </Grid>
+      <Grid item xs={10}>
+        <Button variant="contained" onClick={() => handleSubmit()}>
+          Save
+        </Button>
+      </Grid>
     </Grid>
   );
 };
